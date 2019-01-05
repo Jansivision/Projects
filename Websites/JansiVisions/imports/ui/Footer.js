fd0a71a32189e096c312818db31b66972a9ea2c9
Alvin <mwewamwewa1699@gmail.com>
2019-01-06 00:37:43 +0200

    services page and about page css

--- Websites/JansiVisions/imports/ui/Footer.js
+++ Websites/JansiVisions/imports/ui/Footer.js
@@ -4,8 +4,72 @@ class Footer extends Component{
 
 	render(){
 		return(
-			<div id='footer'>
-				<p> Copyright &copy; JANSI VISIONS 2018 </p> 
+			<div>
+						<div className="footers">
+			<footer class="page-footer font-small ">
+      <div className="container">
+
+<div class="row">
+    <div class="col-sm-5">
+    <h2 >Jansi Visions</h2>
+				<h3>
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.
+
+Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
+				</h3>
+    </div>
+    <div class="col-sm-7 slideanim">
+      <div class="row">
+      <h1>Navigation</h1>
+	  <p><a href='/documentation'>- Documentation</a></p>
+	  
+  <p><a href='/about'>- About</a></p>
+  <p><a  href='/contact'>- Contact</a></p>
+  <p><a  href='/servies'>- Services</a></p>
+      </div>
+    </div>
+  </div>
+			  </div>
+
+
+
+    <ul class="list-unstyled list-inline text-center">
+      <li class="list-inline-item">
+        <a class="btn-floating btn-fb mx-1">
+          <i class="fa fa-facebook"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-tw mx-1">
+          <i class="fa fa-twitter"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-gplus mx-1">
+          <i class="fa fa-google-plus"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-li mx-1">
+          <i class="fa fa-linkedin"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-dribbble mx-1">
+          <i class="fa fa-dribbble"> </i>
+        </a>
+      </li>
+    </ul>
+<div id="br"> </div>
+<hr/>
+    <div class="footer-copyright text-center py-3"> <h2>© 2018 Copyright:
+      <a href="/">jansivisions</a></h2>
+    </div>
+
+
+</footer>
+
+			</div>
 			</div>
 
 		)
--- Websites/JansiVisions/imports/ui/Services.js
+++ Websites/JansiVisions/imports/ui/Services.js
@@ -4,39 +4,142 @@ import React, {Component} from 'react';
 class Services extends Component{
 	render(){
 		return(
-		<div id="white">
-			<h2 class="head">Services We Offer</h2>
-			<h4 id="white">We at Jansi Visions would like to Introduce to you the </h4><h3>"Visions Project"</h3><p>The biggest wave of the 21st Century.</p>
-			<div class="parallax3">
-			<div class="container">
-				<div class="row" id="white">
-					<div class="col-sm-12" id="web3">
-						<h1 class="bg-success">Web Design</h1>
-						<h4 class="bg-success text-justify">Reach more people with a beautifully designed device responsive website.We design and develop websites for all purposes such as, Function ads, Personal blogs, CV's, Company advertizing sites, and so much more.Our aim is to put you a step further than your competitors and grant you that dream oppotunity to expand beyond your wildest dreams.</h4>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-12" id="devices">
-						<h2>Device Updates</h2>
-						<h3>Dickens Njobvu</h3>
-							<p>Contact Details:</p>
-							<p>Cell phone number: 0977 396 223</p>
-							<p>Email Address : jimmykamanga@gmail.com</p>
-					</div>
-				</div>
-				<div class="row" id="white">
-					<div class="col-sm-12" id="computer">
-						<h2>Computer Repairs</h2>
-						<h3>Alvin Mwewa</h3>
-							<p>Contact Details:</p>
-							<p>Cell phone number: 0977 396 223</p>
-							<p>Email Address : jimmykamanga@gmail.com</p>
-					</div>
-				</div>
-			</div>
-			</div>
-		</div>
+			<div id="Services">
 
+			<div class="container-fluid"><div  class="headservices"><br/><br/><br/><br/><br/><br/>
+			<br/><br/><br/><br/><br/>
+			<h1 id="os">Our Services  </h1><h1 id="yellow">____</h1><p id="mo">The
+					  biggest wave of the 21st Century.</p><p id="mo">We at
+				Jansi Visions would like to Introduce to you
+				 the </p></div>
+				 <br/><br/>
+				 <div id="Servicesrow">
+				 <div class="container-fluid">
+  <div class="row" id="row2">
+    <div class="col-sm-4"><img src="images/Certified-Associate-in-Project-Management-CAPM®.jpg" class="img-circle" width="354" height="354"/></div>
+    <div class="col-sm-4"><h2>"Visions Project"</h2>
+				 <h4>Reach more people 
+					with a beautifully designed device
+					 responsive website.We design and develop
+					  websites for all purposes such as, Function
+					   ads, Personal blogs, CV's, Company advertizing 
+					   sites, and so much more.Our aim is to put you a 
+					   step further than your competitors and grant you 
+					   that dream oppotunity to expand beyond your
+						wildest dreams.</h4></div>
+    
+  </div>
+</div>
+
+				 
+				 </div>
+			  <div class="row" id="Servicesrow">
+				<div class="col-sm-4">
+				<h1 id="glyphicon"><span class="glyphicon glyphicon-erase"></span></h1>
+				<h1>Web Design</h1>
+				
+				 <p>Lorem ipsum dolor sit amet, 
+				consectetuer adipiscing elit
+				. Donec odio. Quisque volutpat 
+				mattis eros. Nullam malesuada erat ut turpis.
+				Suspendisse urna nibh, viverra non, semper 
+				suscipit, posuere a, pede.</p>
+						</div>
+				<div class="col-sm-4">
+				<h1 id="glyphicon"><span class="glyphicon glyphicon-cloud-upload"></span></h1>
+					<h1>Software Updates</h1>
+					<p>Lorem ipsum dolor sit amet, 
+				consectetuer adipiscing elit
+				. Donec odio. Quisque volutpat 
+				mattis eros. Nullam malesuada erat ut turpis.
+				Suspendisse urna nibh, viverra non, semper 
+				suscipit, posuere a, pede.</p>
+				</div>
+				<div class="col-sm-4">
+				<h1 id="glyphicon"><span class="glyphicon glyphicon-globe"></span></h1>
+				<h1>Web Development</h1>
+				
+				<p>Lorem ipsum dolor sit amet, 
+			   consectetuer adipiscing elit
+			   . Donec odio. Quisque volutpat 
+			   mattis eros. Nullam malesuada erat ut turpis.
+			   Suspendisse urna nibh, viverra non, semper 
+			   suscipit, posuere a, pede.</p></div>
+			  </div>
+			</div>
+			<div className="footers">
+			<footer class="page-footer font-small ">
+      <div className="container">
+
+<div class="row">
+    <div class="col-sm-5">
+    <h1 >Jansi Visions</h1>
+				<p>
+				Lorem ipsum dolor sit amet, 
+				consectetuer adipiscing elit
+				. Donec odio. Quisque volutpat 
+				mattis eros. Nullam malesuada erat ut turpis.
+				Suspendisse urna nibh, viverra non,
+				 semper suscipit, posuere a, pede.
+				</p>
+				
+    </div>
+    <div class="col-sm-7 slideanim">
+      <div class="row">
+      <h2>Navigation</h2>
+	  <p><a href='/documentation'>- Documentation</a></p>
+	  
+  <p><a href='/about'>- About</a></p>
+  <p><a  href='/contact'>- Contact</a></p>
+  <p><a  href='/servies'>- Services</a></p>
+      </div>
+    </div>
+  </div>
+			  </div>
+
+
+
+    <ul class="list-unstyled list-inline text-center">
+      <li class="list-inline-item">
+        <a class="btn-floating btn-fb mx-1">
+          <i class="fa fa-facebook"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-tw mx-1">
+          <i class="fa fa-twitter"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-gplus mx-1">
+          <i class="fa fa-google-plus"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-li mx-1">
+          <i class="fa fa-linkedin"> </i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a class="btn-floating btn-dribbble mx-1">
+          <i class="fa fa-dribbble"> </i>
+        </a>
+      </li>
+    </ul>
+<div id="br"> </div>
+<hr/>
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="/">jansivisions</a>
+    </div>
+
+
+</footer>
+
+			</div>
+
+					</div>
+			
+			
 
         )
 	}
